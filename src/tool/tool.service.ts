dd5997617c0e62855d65c0249051420ea6df899f
daniel2dfla <daniel_2dfla@hotmail.com>
2024-03-07 15:28:00 -0300

    Finalização da implantação da inversão de dependência

--- src/tool/tool.service.ts
+++ src/tool/tool.service.ts
@@ -1,101 +1,64 @@
-
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Tool } from './tool';
 import { IToolRepository } from 'src/abstractions/class/itool-repository';
 
 @Injectable()
 export class ToolService {
-   
-    constructor( private readonly repository: IToolRepository ) {}
-        
-    async getAll(): Promise<Tool[]> {
-        const tools = await this.repository.getAll();
-        return tools.map(tool => ({
-            id: tool.id,
-            title: tool.title ,
-            link: tool.link,
-            description: tool.description,
-            tags: tool.tags}))
-    }
-     
-    async findByWord( word: string): Promise<Tool[]> {
-        const tools = await this.repository.getAll()
-        const filteredTools = tools.filter(tool => 
-            tool.id !== undefined &&
-            tool.title !== undefined && 
-            tool.link !== undefined && 
-            tool.description !== undefined && 
-            tool.tags !== undefined
-            )
-        
-        return filteredTools.filter(tool => 
-            tool.id !== undefined &&
-            tool.title.toLowerCase().includes(word) ||
-            tool.link.toLowerCase().includes(word) ||
-            tool.description.toLowerCase().includes(word) ||
-            tool.tags.some(tag => tag.toLowerCase().includes(word))
-        )
-    }
+  constructor(private readonly repository: IToolRepository) {}
 
-    async getById(id: string): Promise<Tool> {
-        const idTool = await this.repository.getById(id)
-        return {
-            id: idTool.id,
-            title: idTool.title,
-            link: idTool.link,
-            description: idTool.description,
-            tags: idTool.tags
-        };
-    }
-
-    async create(tool: Tool): Promise<Tool> {
-        const tools = await this.repository.getAll();
-        const existingTool = tools.find(word => word.title === tool.title);
-        const lengthDescription = tool.description.length > 256;
-        const tagsLength = tool.tags.length <= 0 || tool.tags.length > 8;
-
-        if (existingTool) {
-            throw new Error('Tool already exists');
-        }
-        if (lengthDescription) {
-            throw new Error('Description longer than 256 characters');
-        }
-        if (tagsLength) {
-            throw new Error('Tags does not exist or number of tags greater than 8');
-        }
-        const idTool = await this.repository.create(tool)
-        return {
-            id: idTool._id,
-            title: idTool.title,
-            link: idTool.link,
-            description: idTool.description,
-            tags: idTool.tags
-        };
-    }
-
-    async update(id: string, tool: Tool) {
-        const tools = await this.repository.getAll();
-        const existingTool = tools.find(word => word.title === tool.title)
-        const lengthDescription = tool.description.length > 256
-        const tagsLength = tool.tags.length <= 0 || tool.tags.length > 8;
-
-        if (existingTool) {
-            throw new Error('Tool already exists');
-        }
-        if (lengthDescription) {
-            throw new Error('Description longer than 256 characters');
-        }
-        if (tagsLength) {
-            throw new Error('Tags does not exist or number of tags greater than 8');
-        }
-         return this.repository.update(id, tool);
-    }
+  async getAll(): Promise<Tool[]> {
+    return this.repository.getAll();
     
-    async delete(id: string):Promise<void> {
-        await this.repository.delete(id);
-    }
+  }
 
-    async deleteAll(): Promise<void> {
-        await this.repository.deleteAll();
+  async findByWord(word: string): Promise<Tool[]> {
+    if(word === undefined || word === null) {
+        throw new BadRequestException('Word is required');
     }
+    const tools = await this.repository.findByWord(word);
+    
+    if(tools.length === 0 || tools == undefined){
+        return []
+    }
+    return tools
+  }
+
+  async getById(id: string): Promise<Tool> {
+    return this.repository.getById(id);
+  }
+
+  async create(tool: Tool): Promise<Tool> {
+    await this.validateTool(tool);
+    return this.repository.create(tool)
+  }
+
+  async update(id: string, tool: Tool) {
+    await this.validateTool(tool);
+    return  this.repository.update(id, tool);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+
+  async deleteAll(): Promise<void> {
+    await this.repository.deleteAll();
+  }
+
+  private async validateTool(tool: Tool): Promise<void> {
+    const tools = await this.repository.getAll();
+    const existingTool = tools.find((word) => word.title === tool.title);
+    const lengthDescription = tool.description.length > 256;
+    const tagsLength = tool.tags.length <= 0 || tool.tags.length > 8;
+
+    if (existingTool) {
+      throw new BadRequestException('Tool already exists');
+    }
+    if (lengthDescription) {
+      throw new BadRequestException('Description longer than 256 characters');
+    }
+    if (tagsLength) {
+      throw new BadRequestException('Tags does not exist or number of tags greater than 8');
+    }
+  }
 }
